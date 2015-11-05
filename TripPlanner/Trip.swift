192653767f455f43f4a0f70ffe54013665755be3
leslie kimm <leslie.kimm@gmail.com>
2015-11-04 16:20:52 -0800

    Add Trip and Waypoint entities

---
+++ TripPlanner/Trip.swift
@@ -0,0 +1,18 @@
+//
+//  Trip.swift
+//  TripPlanner
+//
+//  Created by Leslie Kim on 11/4/15.
+//  Copyright © 2015 Leslie Kim. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Trip: NSManagedObject {
+    @NSManaged var name: String?
+
+// Insert code here to add functionality to your managed object subclass
+
+}
---
+++ TripPlanner/Waypoint.swift
@@ -0,0 +1,19 @@
+//
+//  Waypoint.swift
+//  TripPlanner
+//
+//  Created by Leslie Kim on 11/4/15.
+//  Copyright © 2015 Leslie Kim. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Waypoint: NSManagedObject {
+    @NSManaged var name: String?
+    @NSManaged var longitude: String?
+    @NSManaged var latitude: String?
+// Insert code here to add functionality to your managed object subclass
+
+}
